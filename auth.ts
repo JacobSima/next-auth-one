b53ad7fe2df087e2c3fb412e2ed732d7d4d8314f
Jacob K Sima <simajacob2011@gmail.com>
2025-06-07 13:26:34 +0200

    Section-02: Enable callback uses and extend token and session data

--- auth.ts
+++ auth.ts
@@ -3,9 +3,50 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
- 
-export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth({
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma/client";
+
+export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db),
-  session: {strategy: "jwt"},
+  session: { strategy: "jwt" },
   ...authConfig,
 });
--- data/user.ts
+++ data/user.ts
@@ -1,17 +1,21 @@
 import { db } from "@/lib/db";
 
-export const getUserByEmail = async(email: string) => {
+export const getUserByEmail = async (email: string) => {
   try {
-    const user = await db.user.findUnique({where: {email}});
+    const user = await db.user.findUnique({ where: { email } });
     return user;
   } catch (error) {
     return null;
   }
 }
 
-export const getUserById = async(id: string) => {
+export const getUserById = async (id?: string) => {
+  if (!id) {
+    return null;
+  }
+  
   try {
-    const user = await db.user.findUnique({where: {id}});
+    const user = await db.user.findUnique({ where: { id } });
     return user;
   } catch (error) {
     return null;
